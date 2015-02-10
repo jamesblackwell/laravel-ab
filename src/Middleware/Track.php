23f56c67702a3d17bcc212183267aa05627aaacd
Rob Mazur <robmazur@gmail.com>
2015-02-10 16:08:01 -0500

    Add app instance in constructor

--- src/Middleware/Track.php
+++ src/Middleware/Track.php
@@ -2,8 +2,27 @@
 
 use Closure;
 use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Contracts\Foundation\Application;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Track implements Middleware {
+
+    /**
+     * The application implementation.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
 
     /**
      * Handle an incoming request.
